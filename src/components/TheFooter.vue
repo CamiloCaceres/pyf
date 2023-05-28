4ba6107ac4a69eb0abebae7da2963f2bf539e190
camilo caceres <camiloac97@gmail.com>
2023-05-28 10:03:45 -0500

    ui: change footer icons

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,36 +1,36 @@
 <script setup lang="ts">
-import { availableLocales, loadLanguageAsync } from '~/modules/i18n'
+// import { availableLocales, loadLanguageAsync } from '~/modules/i18n'
 
-const { t, locale } = useI18n()
+const { t } = useI18n()
 
-async function toggleLocales() {
-  // change to some real logic
-  const locales = availableLocales
-  const newLocale = locales[(locales.indexOf(locale.value) + 1) % locales.length]
-  await loadLanguageAsync(newLocale)
-  locale.value = newLocale
-}
+// async function toggleLocales() {
+//   // change to some real logic
+//   const locales = availableLocales
+//   const newLocale = locales[(locales.indexOf(locale.value) + 1) % locales.length]
+//   await loadLanguageAsync(newLocale)
+//   locale.value = newLocale
+// }
 </script>
 
 <template>
   <nav flex="~ gap-4" mt-6 justify-center text-xl>
     <RouterLink icon-btn to="/" :title="t('button.home')">
-      <div i-carbon-campsite />
+      <div i-carbon-home />
     </RouterLink>
 
     <button icon-btn :title="t('button.toggle_dark')" @click="toggleDark()">
       <div i="carbon-sun dark:carbon-moon" />
     </button>
 
-    <a icon-btn :title="t('button.toggle_langs')" @click="toggleLocales()">
+    <!-- <a icon-btn :title="t('button.toggle_langs')" @click="toggleLocales()">
       <div i-carbon-language />
-    </a>
+    </a> -->
 
-    <RouterLink icon-btn to="/about" :title="t('button.about')">
+    <!-- <RouterLink icon-btn to="/about" :title="t('button.about')">
       <div i-carbon-dicom-overlay />
-    </RouterLink>
+    </RouterLink> -->
 
-    <a icon-btn rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
+    <a icon-btn rel="noreferrer" href="https://github.com/CamiloCaceres/pyf" target="_blank" title="GitHub">
       <div i-carbon-logo-github />
     </a>
   </nav>
